1fbe6cf38a2d887751fe1fc14aee008ce9f3caff
MHeggy <122556107+MHeggy@users.noreply.github.com>
2023-09-29 14:12:11 -0400

    More js!

--- main.js
+++ main.js
@@ -1,112 +1,67 @@
-/* Author: Miles Hegeduis
- * Date: 09/28/2023
- * Name: main.js
- * Description: this is the javascript file for handling client-side scripting.
- */
+/*
+/ Author: Miles Hegeduis
+/ Date: 09/20/2023
+/ File: main.js
+/ Description: This file contains javascript code that displays a list of countries of a selected continent.
+/*
 
-/******************************************************************************
- * Handle key press event.
- * This function makes asynchronous HTTP request using the XMLHttpRequest object.
- * It passes a name to population.php for processing.
- *****************************************************************************/
-function handlekeyup(e) {
-    //retrieve user input from the text box
-    let name = trim(document.getElementById('name').value);
-    if(name === "") {
-        error("");
-        return;
+
+/* Handle window onload event. It creates a selection list of seven continents.
+*/
+window.onload = function () {
+    // load continents from an external json file
+    let continents = JSON.parse(loadJSON("continents.json"));
+    //Grabbing the DOM of the selection named "continents" and setting it to variable continentSelect.
+    let continentSelect = document.getElementById("continents");
+
+    //create the dropdown list for the continents
+    let _html_select = "<option selected='selected' disabled='disabled'>Select a Continent </option>";
+    for (let continent in continents) {
+        _html_select += "<option value='" + continent + "'>" + continents[continent] + "</option>";
     }
-	
-    //add your code here to process ajax requests and handle server's responses
+    continentSelect.innerHTML = _html_select;
 
-    //creating new XMLHttpRequest() object.
-    let xhr = new XMLHttpRequest();
 
-    //opening asynchronous request.
-    xhr.open("GET", "population.php?name=" + name, true);
 
-    //handle the server's responses.
-    xhr.onload = function() {
-        let population = JSON.parse(xhr.responseText);
-        console.log(xhr.responseText);
-        //checking to see if population contains the name of the user input.
-        if (population.hasOwnProperty('metadata')) {
-            display(population);
-        }
-        else {
-            //error handling.
-            error("Name not found.");
-            clear();
-        }
-    }//end of xhr.onload anonymous function.
-    //sending the AJAX request.
-    xhr.send(null);
+    //load countries data from an external json file.
+    let countries = JSON.parse(loadJSON("countries.json"));
+
+
+    //Now getting the value from that continentSelect dropdown.
+    let continentValue = continentSelect.value;
+    //handle change event of the dropdown list and call the display method.
+    continentSelect.onchange = function () {
+        //updating the continent value that is selected by user.
+        continentValue = continentSelect.value;
+        //calling the display method when the user changes the selection of the continent.
+        display(countries, continentValue);
+    }
 }
 
-/*
- * This function accepts a JSON object containing population information
- * and display it in an HTML table. This function get invoked by the handlekeyup function
- * when the country/region name is found in the database.
- * 
- */
-function display(population) {
-    //temporary debugging code.
-    console.log("Display function called");
-    console.log(population);
-    //initializing empty string "HTMLDiv" to append div and population information to.
-    let HTMLDiv = "";
-    //Access the country name from the metadata:
-    let countryName = population.metadata.name;
-    //add your code here to retrieve data from an JSON object and then display them
-    for(let year in population) {
-        const popData = population[year];
-        if (year !== "metadata") {
-        HTMLDiv += "<div class='row'>";
-        HTMLDiv += "<div>" + year + "</div>";
-        HTMLDiv += "<div>" + popData[countryName] + "</div>";
-        HTMLDiv += "<div>" + popData["World"] + "</div>";
-        HTMLDiv += "</div>";
-        }//end of if statement.
-    }//end of for in loop.
-    //set the innerHTML of population results with the HTMLDiv element that I created.
-    document.getElementById("population-results").innerHTML = HTMLDiv;
-    //set the innerHTML of country span tag in the index.php.
-    document.getElementById("country").innerHTML = population.metadata.name;
-    //set the innerHTML of country code span tag in index.php.
-    document.getElementById("code").innerHTML = population.metadata.code;
-}//end of display() function.
 
-/*
- * This function clears the population. The function is invoked by the handlekeyup function if
- * the country name a user has entered is not found in the database.
- */
-function clear() {
-    //add your code here to clear the population data.
-        //clear the population-results div.
-        document.getElementById("population-results").innerHTML = "";
-        //clear the country code div.
-        document.getElementById("code").innerHTML = "";
-        //clear the country name div.
-        document.getElementById("country").innerHTML = "";
-}
+/* This function takes a json object of countries and a continent as the parameters.
+*  It filters the json document with a continent then appends a row to
+*  the div block for each country.
+*/
+    function display(countries, continentValue) {
+        //Establishing _html_div variable to empty string to append my divs to.
+        let _html_div = "";
+        //start of for...in loop.
+        for (let countryKey in countries) {
+            //establishing country variable as a constant.
+            const country = countries[countryKey];
+            //need an if statement for the proper countries to display based upon the continent selection.
+            if (country.continent === continentValue) {
+                //appending the div class row to the first _html_div | initial div.
+                _html_div += "<div class='row'>";
+                _html_div += "<div>" + country.name + "</div>";
+                _html_div += "<div>" + country.capital + "</div>";
+                _html_div += "<div>" + country.currency.join(", ") + "</div>";
+                _html_div += "<div>" + country.languages.join(", ") + "</div>";
+                _html_div += "<div>" + country.phone.join(", ") + "</div>";
+            }//end of if statement
 
-//This function displays an error message in the div block with the id of "message".
-function error(message) {
-    //add your code here to display an error message.
-    document.getElementById('message').innerHTML = message;
-}
-
-/*
-* A home-made trim function that removes leading and
- * trailing whitespace characters from a string
- */
-function trim(str) {
-    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-}
-
-/*
-* A home-made function that displays a number with commas as thousands separators
- */
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+            _html_div += "</div>"; //closing off the initial div.
+            document.getElementById("countries").innerHTML = _html_div;
+        }//end of for...in loop
+    }//end of display function
